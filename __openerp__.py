eacac7964b5894f734c442d397d67c1ca178e268
Luc Bonjean <luc@lubon.be>
2016-08-12 21:42:16 +0200

    VErsion  umber

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Entity SMS",
-    'version': "2.9.11",
+    'version': "2.9.12",
     'author': "Sythil, Lubon bvba",
     'category': "Tools",
     'summary': "Allows 2 way sms conversations between leads/partners using the twilio gateway",
